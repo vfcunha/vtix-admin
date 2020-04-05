f0c0cc49629db040a4dca0af3da0cdc2fcb44de5
Vitor Cunha <vitorfcunha@gmail.com>
2020-04-05 12:38:25 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app >
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  computed: {
+    config: {
+      get () {
+        return this.$store.getters.layout
+      }
+    }
+  },
+  data: () => ({
+    layout: 'div'
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    Dashboard
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    data: () => ({
+      
+    })
+  }
+</script>
